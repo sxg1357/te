90d0bcb5b8c08069d8454067b3dea225a089295a
sxg <1353693123@qq.com>
2022-08-01 14:07:03 +0800

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '92e839d128082488049425acc7d9a78602b42919',
+        'name' => 'socket/ms',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'socket/ms' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '92e839d128082488049425acc7d9a78602b42919',
+            'dev_requirement' => false,
+        ),
+    ),
+);
