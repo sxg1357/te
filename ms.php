0851a03dd3446cecb80506cc44c9635bdea6174f
sxg <1353693123@qq.com>
2022-08-03 21:06:57 +0800

    优化

--- ms.php
+++ ms.php
@@ -13,7 +13,7 @@ $server = new Socket\Ms\Server("tcp://127.0.0.1:9501");
 $server->on("connect", function (\Socket\Ms\Server $Server, \Socket\Ms\TcpConnections $TcpConnections) {
     fprintf(STDOUT, "有客户端连接了\n");
 });
-$server->on("receive", function ($msg, \Socket\Ms\TcpConnections $connections) {
+$server->on("receive", function (\Socket\Ms\Server $Server, $msg, \Socket\Ms\TcpConnections $connections) {
     fprintf(STDOUT, "有客户发送数据:%s\n", $msg);
     $connections->writeSocket($connections->_socketFd, $msg);
 });
